ee445b50298fd13a9a58231312357fb6272c762e
xavyjs <xavyjs61@gmail.com>
2025-06-25 01:27:02 +0800

    refactor(gilded_rose): decomposing loop and iter instance

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -14,36 +14,38 @@ from item import Item
         maintenance
 """
 
-
 def update_quality(items: Iterable[Item]):
     for item in items:
-        if (
-            item.name != "Aged Brie"
-            and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        ):
-            if item.quality > 0:
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.quality = item.quality - 1
+        update_quality_single(item)
+
+def update_quality_single(item: Item):
+    if (
+        item.name != "Aged Brie"
+        and item.name != "Backstage passes to a TAFKAL80ETC concert"
+    ):
+        if item.quality > 0:
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.quality = item.quality - 1
+    else:
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in < 11:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+    if item.name != "Sulfuras, Hand of Ragnaros":
+        item.sell_in = item.sell_in - 1
+    if item.sell_in < 0:
+        if item.name != "Aged Brie":
+            if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
+            else:
+                item.quality = item.quality - item.quality
         else:
             if item.quality < 50:
                 item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
