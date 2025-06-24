b3640c872066730ebb277405b8dd9523f4657c01
xavyjs <xavyjs61@gmail.com>
2025-06-25 01:44:42 +0800

    refactor(gilded_rose): Replacing Item Strings by CONSTANTS to avoid frequent typos

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -14,24 +14,29 @@ from item import Item
         maintenance
 """
 
+# Item types
+AGED_BRIE = "Aged Brie"
+BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+
 def update_quality(items: Iterable[Item]):
     for item in items:
         update_quality_single(item)
 
 def update_quality_single(item: Item):
-    if item.name != "Sulfuras, Hand of Ragnaros":
+    if item.name != SULFURAS:
         item.sell_in = item.sell_in - 1
     if (
-        item.name != "Aged Brie"
-        and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        item.name != AGED_BRIE
+        and item.name != BACKSTAGE_PASSES
     ):
         if item.quality > 0:
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != SULFURAS:
                 item.quality = item.quality - 1
     else:
         if item.quality < 50:
             item.quality = item.quality + 1
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            if item.name == BACKSTAGE_PASSES:
                 if item.sell_in < 10:
                     if item.quality < 50:
                         item.quality = item.quality + 1
@@ -39,10 +44,10 @@ def update_quality_single(item: Item):
                     if item.quality < 50:
                         item.quality = item.quality + 1
     if item.sell_in < 0:
-        if item.name != "Aged Brie":
-            if item.name != "Backstage passes to a TAFKAL80ETC concert":
+        if item.name != AGED_BRIE:
+            if item.name != BACKSTAGE_PASSES:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != SULFURAS:
                         item.quality = item.quality - 1
             else:
                 item.quality = item.quality - item.quality
